7131375be4a7b8081f1e2db567b2d42106a73266
Cristina <kamuthucristina@gmail.com>
2025-03-18 05:32:57 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const element = document.querySelector("#main");
+element.remove("#main");
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Cristina Nyawira is the champion"
+
